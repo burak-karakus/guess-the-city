c85c5dd3a8b4900610edde377abecc345d7a96d5
blackbird <sadiburakk@gmail.com>
2023-03-05 00:37:49 +0300

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='min-h-screen bg-gradient-to-b from-green-500 to-pink-500'>
+     
+    </div>
+  )
+}
+
+export default App
