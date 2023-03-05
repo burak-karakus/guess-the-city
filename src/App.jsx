8bb27a527e31efedd25126eed189efcf09059145
blackbird <sadiburakk@gmail.com>
2023-03-05 12:52:06 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,14 @@ const App = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='min-h-screen bg-gradient-to-b from-green-500 to-pink-500'>
-       <div>test</div>
-       
-       <Map/>
+    <div>
+      <Map />
     </div>
+    //<div className='min-h-screen bg-gradient-to-b from-green-500 to-pink-500'>
+       //<div>test</div>
+       
+       
+    //</div>
   )
 }
 
