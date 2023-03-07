da11e5f467d43cbacaebaf52ce15c9e27c8305fc
blackbird <sadiburakk@gmail.com>
2023-03-07 18:07:17 +0300

    framer-motion opacity the map added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Map from './components/Map'
+import Choose from './components/Choose';
 import { cities } from './data/cities';
 
 
@@ -12,6 +13,7 @@ const App = () => {
 
   return (
     <div>
+      <Choose />
       <Map /> 
     </div>
     //<div className='min-h-screen bg-gradient-to-b from-green-500 to-pink-500'>
--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -1,6 +1,8 @@
- import { AnimatePresence, MotionConfig } from "framer-motion";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { GameContext } from "../context/GameContext";
+import { AnimatePresence, motion } from "framer-motion";
+
+
 
  const Choose = ({setGameType}) => {
     const [showModal, setShowModal] = useState(true); 
@@ -26,19 +28,20 @@ import { GameContext } from "../context/GameContext";
 
     return (
          <>
-            <AnimatePresence exitBeforeEnter={true}>
-                {showModal && (
+            <AnimatePresence wait={true}>
+                { showModal && (
                 <motion.div
                     className="absolute inset-0 flex justify-center items-center z-10 w-full h-full backdrop-blur-md bg-white/30"
                     variants={backdrop}
                     initial="hidden"
                     animate="visible"
                     >
+                        <motion.div variants={modal} className="w-96 h-48 p-4 rounded-lg">
+
+                        </motion.div>
 
                 </motion.div>     
-                )
-
-                }
+                )}
 
             </AnimatePresence>
          </>
