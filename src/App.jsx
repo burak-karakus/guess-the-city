605f6e4b1fa356f30dac39f94c4752431cd04927
blackbird <sadiburakk@gmail.com>
2023-03-12 15:12:13 +0300

    keyboard and mouse svgs are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React, {useState } from 'react';
+import React, {useContext, useState } from 'react';
 
 import Map from './components/Map'
 import Choose from './components/Choose';
 import { cities } from './data/cities';
+import { GameContext } from './context/GameContext';
 
 
 const App = () => {
   const [gameType, setGameType] = useState("");
+  const [cityToBeGuessed, setCityToBeGuessed] = useState("");
   const generateRandomCity = () => {
     return cities.features[Math.floor(Math.random() * cities.features.length)].properties.NAME;
   }
+  const { countdown, setCountdown, isGameOn, clickedCity, setClickedCity, typedCity, setTypedCity, score, setScore} = useContext(GameContext);
 
   return (
     <div>
--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { GameContext } from "../context/GameContext";
 import { AnimatePresence, motion } from "framer-motion";
-import { TYPING } from "../constants";
+import { CLICKING, TYPING } from "../constants";
 
 
 
@@ -52,8 +52,14 @@ import { TYPING } from "../constants";
                             <h3 className="text-center text-2xl font-bold text-primaryLight"> Guess The City</h3>
                             <h2 className="text-center text-primaryLight font-bold "> by</h2>
                             <div className="flex justify-around items-center mt-2">
-                                <button onClick={() => chooseHandler(TYPING)} className="choose-btn">Typing</button>
-                                <button>b</button>
+                                <button onClick={() => chooseHandler(TYPING)} className="choose-btn">
+                                    <svg className="fill-primaryLight mx-auto h-10 w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M64 64C28.7 64 0 92.7 0 128V384c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H64zm16 64h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM64 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V240zm16 80h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V336c0-8.8 7.2-16 16-16zm80-176c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V144zm16 80h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V240c0-8.8 7.2-16 16-16zM160 336c0-8.8 7.2-16 16-16H400c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V336zM272 128h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM256 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V240zM368 128h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H368c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM352 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H368c-8.8 0-16-7.2-16-16V240zM464 128h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H464c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM448 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H464c-8.8 0-16-7.2-16-16V240zm16 80h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H464c-8.8 0-16-7.2-16-16V336c0-8.8 7.2-16 16-16z"/></svg>
+                                    <div>Typing</div>
+                                </button>
+                                <button onClick={() => chooseHandler(CLICKING)} className="choose-btn">
+                                    <svg className="fill-primaryLight mx-auto h-10 w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M0 192H176V0H160C71.6 0 0 71.6 0 160v32zm0 32V352c0 88.4 71.6 160 160 160h64c88.4 0 160-71.6 160-160V224H192 0zm384-32V160C384 71.6 312.4 0 224 0H208V192H384z"/></svg>
+                                    <div>Typing</div>
+                                </button>
                             </div>
                         </motion.div>
 
